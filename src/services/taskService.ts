c47ed13a28e64c55995018f49420e6ebebaf3087
Kevin A Hernandez Garcia <kevinalbertohernandezgarcia@gmail.com>
2025-07-26 00:17:03 -0500

    added the actual link for the live backend

--- src/services/taskService.ts
+++ src/services/taskService.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import type { Task, Subtask } from '../../server/src/types';
 
-const API_URL = 'http://localhost:3001/api/tasks';
+const API_URL = 'https://project-todo-lbqi.onrender.com/api/tasks';
 
 export const getTasks = async (): Promise<Task[]> => {
   const response = await axios.get<Task[]>(API_URL);
